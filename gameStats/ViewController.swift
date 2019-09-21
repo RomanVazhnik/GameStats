4e9ac2a5fcd870c1cdcfc1322b122111383ca969
Roman <vazhnik.roma@mail.ru>
2019-09-21 18:29:13 +0300

    Initial Commit

---
+++ gameStats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gameStats
+//
+//  Created by Роман Важник on 21/09/2019.
+//  Copyright © 2019 Роман Важник. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
